cc0438cc2d47fe3608e1caccf7cc4d18e22e6673
Yumejichi <143553417+Yumejichi@users.noreply.github.com>
2026-07-29 18:15:23 -0700

    update urls

--- onthefly/server/config/auth.js
+++ onthefly/server/config/auth.js
@@ -1,37 +1,47 @@
-import GithubStrategy from 'passport-github2'
-import { pool } from './database.js'
+import GithubStrategy from 'passport-github2';
+import { pool } from './database.js';
 
 const options = {
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3001/auth/github/callback'
-}
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: 'https://server-0ml7.onrender.com/auth/github/callback',
+};
 
 const verify = async (accessToken, refreshToken, profile, callback) => {
-    const { _json: { id, name, login, avatar_url } } = profile
-    const userData = {
-        githubId: id,
-        username: login,
-        avatarUrl: avatar_url,
-        accessToken
+  const {
+    _json: { id, name, login, avatar_url },
+  } = profile;
+  const userData = {
+    githubId: id,
+    username: login,
+    avatarUrl: avatar_url,
+    accessToken,
+  };
+
+  try {
+    const results = await pool.query('SELECT * FROM users WHERE username=$1', [
+      userData.username,
+    ]);
+    const user = results.rows[0];
+
+    if (!user) {
+      const result = await pool.query(
+        `INSERT INTO users (githubid, username, avatarurl, accesstoken)
+                VALUES($1,$2,$3,$4) RETURNING *`,
+        [
+          userData.githubId,
+          userData.username,
+          userData.avatarUrl,
+          userData.accessToken,
+        ],
+      );
+      const newUser = result.rows[0];
+      return callback(null, newUser);
     }
+    return callback(null, user);
+  } catch (error) {
+    return callback(error);
+  }
+};
 
-    try {
-        const results = await pool.query('SELECT * FROM users WHERE username=$1', [userData.username])
-        const user = results.rows[0]
-
-        if (!user) {
-            const result = await pool.query(
-                `INSERT INTO users (githubid, username, avatarurl, accesstoken)
-                VALUES($1,$2,$3,$4) RETURNING *`, [userData.githubId, userData.username, userData.avatarUrl, userData.accessToken]
-            )
-            const newUser = result.rows[0]
-            return callback(null, newUser)
-        }
-        return callback(null, user)
-    } catch (error) {
-        return callback(error)
-    }
-}
-
-export const GitHub = new GithubStrategy(options, verify)
+export const GitHub = new GithubStrategy(options, verify);
